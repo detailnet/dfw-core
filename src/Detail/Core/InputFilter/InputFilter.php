26e7b8904db144f234283f0ac4ee06cb88fbc84e
Ivan Wolf <iwolf@detailag.ch>
2014-07-21 13:24:16 +0200

    Implemented uploading of file

---
+++ src/Detail/Core/InputFilter/InputFilter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Detail\Core\InputFilter;
+
+use Zend\InputFilter\InputFilter as Filter;
+
+class InputFilter extends Filter implements
+    InputFilterInterface
+{
+}
---
+++ src/Detail/Core/InputFilter/InputFilterInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Detail\Core\InputFilter;
+
+interface InputFilterInterface
+{
+}
